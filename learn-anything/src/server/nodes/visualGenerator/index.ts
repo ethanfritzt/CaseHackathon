0d88e923059b4d18cc8a31d8db4daf8c1f43699d
Bill Feth <contact@billfeth.com>
2024-10-13 14:24:29 -0400

    WIP image generator

--- learn-anything/src/server/nodes/visualGenerator/index.ts
+++ learn-anything/src/server/nodes/visualGenerator/index.ts
@@ -1,4 +1,7 @@
 import { GraphStateType } from "src/server/state/index";
+import { OpenAI } from "@langchain/openai";
+import fs from 'fs';
+
 export async function createManimVisual(state: GraphStateType): Promise<Partial<GraphStateType>> {
   const { scenes } = state;
   // for each scene, generate visual content with manim
@@ -6,9 +9,48 @@ export async function createManimVisual(state: GraphStateType): Promise<Partial<
     // generate visual content for the scene
     console.log(`Generating manim video for scene ${index}`);
   }));
+}
+
+// save the image to disk
+function saveImage(image_url: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("path/to/image", image_url, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+// define the generate image function
+async function generateImage(scene: string, index: number): Promise<{imagePath: string}> {
+  // generate image for the scene
+  // return the image path
+  console.log(`Generating image for scene ${index}`);
+  
+  return {
+    imagePath: "path/to/image",
+  }
+}
+
+export async function imageGenerator(state: GraphStateType): Promise<Partial<GraphStateType>> {
+  const {scenes} = state;
+  // for each scene, generate images in parallel
+  // should probably modify the scenes to update with the image file path
+  const updatedScenes = await Promise.all(scenes.map(async (scene, index) => {
+    // generate image for the scene
+    console.log(`Generating image for scene ${index}`);
+    const { imagePath } = await generateImage(scene.content, index).resolve();
+
+    return {
+      ...scene,
+      path: imagePath
+    };
+  }));
 
   return {
-    // todo
-    // scenes: scenes,
+    scenes: updatedScenes,
   };
 }
